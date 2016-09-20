b9c7e41848c1e4040b288f3b2eccaa6a8928c893
jeremypress <jeremypress@berkeley.edu>
2016-09-19 23:25:28 -0700

    button works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-console.log(process.env.LIFXKEY);
+var bodyParser = require('body-parser')
 
 //setup template engine
 app.set('view engine', 'ejs');
@@ -9,27 +9,63 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'))
 
-app.get('/', function(req, res) {
-  var lightState = null;
-  const options = {
-    url: 'https://api.lifx.com/v1/lights/all',
-    headers: {
-      'Authorization': 'Bearer ' + process.env.LIFXKEY
-    }
-  };
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var options = {
+  url: undefined,
+  headers: {
+    'Authorization': 'Bearer ' + process.env.LIFX
+  }
+};
+
+var lightState = null;
+
+function extractLightStatus() {
+  if (lightState) {
+    return "on";
+  } else {
+    return "off";
+  }
+}
+
+app.get('/', function(req, res) {
+  options.url = 'https://api.lifx.com/v1/lights/all';
+  //getting light state
   request(options, function(error, response, body) {
     if (error) {
       console.log("ERROR");
       return;
     }
-    lightState = JSON.parse(body)[0]; //only handling one light right now
-    if (lightState.power === 'off') {
-      console.log('light is off!')
+    console.log(body)
+    lightState = JSON.parse(body)[0].power;
+    console.log(lightState)
+    if (lightState === 'off') {
+      lightState = false;
     } else {
-      console.log('light is on!')
+      console.log('light is on')
+      lightState = true;
     }
-    res.render('index', {power : lightState.power});
+    res.render('index', {power : extractLightStatus()});
+  });
+});
+
+app.post('/api/toggle_power', function(req, res) {
+  console.log('received');
+  options.url = "https://api.lifx.com/v1/lights/all/toggle";
+
+  request.post(options, function(error, response, body) {
+    if (error) {
+      console.log("ERROR");
+      return;
+    }
+    status = JSON.parse(body).results[0].status;
+    if (status === "ok") {
+      lightState = !lightState;
+      console.log(lightState)
+      res.render('index', {power : extractLightStatus()});
+    }
   });
 });
 
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  $('#toggle_power').on('click', function() {
+    $.ajax({ //add some kind of data!
+      type: "POST",
+      url: '/api/toggle_power',
+      data: {'data': 'power'},
+      success: function (data) {
+        console.log('hi')
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+             console.log('error ' + textStatus + " " + errorThrown);
+      }
+    });
+  });
 });
