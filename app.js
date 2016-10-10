08689f295ec290aaac7c7c2074fe8df5778748a2
jeremypress <jeremypress@berkeley.edu>
2016-10-09 20:52:41 -0700

    addin back end functionality

--- app.js
+++ app.js
@@ -1,76 +1,149 @@
+// Requires
 var express = require('express');
 var app = express();
 var request = require('request');
 var bodyParser = require('body-parser')
 
-//setup template engine
+
+// Setup
+// setup template engine
 app.set('view engine', 'ejs');
-
-//static files
 app.use(express.static('./public'))
-
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// State Variables
 var options = {
-  url: undefined,
-  headers: {
-    'Authorization': 'Bearer ' + process.env.LIFX
-  }
+    url: undefined,
+    headers: {
+        'Authorization': 'Bearer ' + process.env.LIFX
+    }
 };
-
+console.log(process.env.LIFX)
 var lightState = null;
-
-function extractLightStatus() {
-  if (lightState) {
-    return "on";
-  } else {
-    return "off";
-  }
+var stateManager = {
+    timerId: 0,
+    timers : {},
+    alarms: []
 }
 
-app.get('/', function(req, res) {
-  options.url = 'https://api.lifx.com/v1/lights/all';
-  //getting light state
-  request(options, function(error, response, body) {
-    if (error) {
-      console.log("ERROR");
-      return;
-    }
-    console.log(body)
-    lightState = JSON.parse(body)[0].power;
-    console.log(lightState)
-    if (lightState === 'off') {
-      lightState = false;
+// Helper Functions
+function extractLightStatus() {
+    if (lightState) {
+        return "on";
     } else {
-      console.log('light is on')
-      lightState = true;
+        return "off";
     }
-    res.render('index', {power : extractLightStatus()});
-  });
+}
+
+function endTimer() {
+    options.url = 'https://api.lifx.com/v1/lights/all/effects/pulse';
+    stateManager.timerId += 1;
+    body = {
+        color: 'blue',
+        cycles: 3.0,
+        period: .5,
+        power_on: true
+    }
+    var timerOptions = {
+        method: 'post',
+        body: body, // Javascript object
+        json: true, // Use,If you are sending JSON data
+        url: options.url,
+        headers: options.headers
+    }
+    request.post(timerOptions, function(error, response, body) {
+        if (error) {
+            console.log("ERROR");
+            return;
+        }
+        console.log('success');
+        // @todo, respond to the client that the timer happened
+    });
+}
+
+
+// API Endpoints
+
+app.get('/', function(req, res) {
+    options.url = 'https://api.lifx.com/v1/lights/all';
+    //getting light state
+    request(options, function(error, response, body) {
+        if (error) {
+            console.log("ERROR");
+            return;
+        }
+        lightState = JSON.parse(body)[0].power;
+        if (lightState === 'off') {
+            lightState = false;
+        } else {
+            lightState = true;
+        }
+        console.log('light is on: ' + lightState);
+        res.render('index', {power : extractLightStatus()});
+    });
 });
 
 app.post('/api/toggle_power', function(req, res) {
-  console.log('received');
-  options.url = "https://api.lifx.com/v1/lights/all/toggle";
+    console.log('received');
+    options.url = "https://api.lifx.com/v1/lights/all/toggle";
 
-  request.post(options, function(error, response, body) {
-    if (error) {
-      console.log("ERROR");
-      return;
+    request.post(options, function(error, response, body) {
+        if (error) {
+            console.log("ERROR");
+            return;
+        }
+        status = JSON.parse(body).results[0].status;
+        if (status === "ok") {
+            lightState = !lightState;
+            console.log('light is on: ' + lightState);
+            res.render('index', {power : extractLightStatus()});
+        }
+    });
+});
+
+app.post('/api/set_timer', function(req, res) {
+    console.log('received');
+    options.url = "https://api.lifx.com/v1/lights/all/toggle";
+    var time = req.body.time * 60000; //convert to milliseconds
+    console.log(time)
+    setTimeout(endTimer, time);
+
+    var response = {
+        status: 200,
+        success: 'Updated Successfully',
+        timerId: stateManager.timerId
     }
-    status = JSON.parse(body).results[0].status;
-    if (status === "ok") {
-      lightState = !lightState;
-      console.log(lightState)
-      res.render('index', {power : extractLightStatus()});
-    }
-  });
+
+    res.end(JSON.stringify(response));
+});
+
+app.post('/api/set_color', function(req, res) {
+    console.log('hi')
+    options.url = "https://api.lifx.com/v1/lights/all/state";
+    options.json = {
+        color: 'green'
+    };
+    request.put(options, function(error, response, body) {
+        if (error) {
+            console.log("ERROR");
+            return;
+        }
+        // status = JSON.parse(body).results[0].status;
+        // console.log(status)
+        // if (status === "ok") {
+        //     var response = {
+        //         status: 200,
+        //     };
+        //
+        //     res.end(JSON.stringify(response));
+        // }
+    });
 });
 
 
 
 
-//listening on port 3000
+// Listen
 app.listen(3000);
+console.log('Bright Light Running!')
--- public/assets/script.js
+++ public/assets/script.js
@@ -12,4 +12,53 @@ $( document ).ready(function() {
       }
     });
   });
+
+  $('#set_timer').on('click', function() {
+    var time = $('#time').val();
+    //var power = $('#timer_power').val();
+    console.log('clicked');
+    $.ajax({ //add some kind of data!
+      type: "POST",
+      url: '/api/set_timer',
+      data: {'time': time},
+      success: function (data) {
+        console.log('success');
+        updateTimer(time * 60);
+        $('.timer-display').text(time * 60)
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+             console.log('error ' + textStatus + " " + errorThrown);
+      }
+    });
+  });
+
+  $('#set_color').on('click', function() {
+      console.log('clicked');
+      $.ajax({ //add some kind of data!
+        type: "POST",
+        url: '/api/set_color',
+        data: {'color': "color"},
+        success: function (data) {
+          console.log('success');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+               console.log('error ' + textStatus + " " + errorThrown);
+        }
+      });
+
+  })
+
+  function updateTimer(time) {
+    setInterval(function() {
+      console.log(time)
+      var timer = $('.timer-display');
+      if (time === 0) {
+          timer.text(0);
+        clearInterval();
+      } else {
+          time -= 1;
+          timer.text(time)
+      }
+    }, 1000);
+  }
 });
